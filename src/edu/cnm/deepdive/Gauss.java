2193da977945beb85c2a8543a07c46107b91d21a
Kevin Simms <ksimms2@cnm.edu>
2019-01-22 15:36:14 -0700

    Read upperLimit from Command line.

--- src/edu/cnm/deepdive/Gauss.java
+++ src/edu/cnm/deepdive/Gauss.java
@@ -5,9 +5,12 @@ public class Gauss {
   public static final int DEFAULT_UPPER_LIMIT = 100;
 
   public static void main(String[] args) {
-    
 
     int upperLimit = DEFAULT_UPPER_LIMIT;
+    if (args.length > 0) {
+      upperLimit = Integer.parseInt(args [0]);
+    }
+
     long sum = triangleSum(upperLimit);
 
     System.out.printf("Sum from 1 to %,d = %,d%n", upperLimit, sum); // % is a place holder, d is decimal, n is new line.
